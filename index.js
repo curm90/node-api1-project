777a7bc22a1040fc57d45812b1d502afb755f75e
Liam Sutton <devliam90@gmail.com>
2019-10-28 12:18:57 +0000

    install deps, instantiate app

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // implement your API here
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('*', handleDefaultRequest);
+
+function handleDefaultRequest(req, res) {
+  res.json('hello world');
+}
+
+app.listen(port || 3000, () => {
+  console.log('listening on ' + (port || 3000));
+});
